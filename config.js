88b4c0c5c34d5653516e72235a3226538c2cc168
gongz1023 <3818404230@qq.com>
2026-05-22 23:13:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  siteName: '龚仔的摄影日记',
+  siteName: '仔仔的摄影日记',
   siteTitle: '记录生活，定格美好',
   siteDescription: '用镜头捕捉每一个值得珍藏的瞬间',
   footer: {
-    html: '© 2026 龚仔的摄影日记 | <a href="https://v.douyin.com/Hc74k9FgjPY/" target="_blank">@龚仔仔仔仔</a>'
+    html: '© 2026 仔仔的摄影日记 | <a href="https://v.douyin.com/Hc74k9FgjPY/" target="_blank">@仔仔</a>'
   },
   contacts: {
     douyin: 'https://v.douyin.com/Hc74k9FgjPY/',
